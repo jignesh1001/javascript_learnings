a5fa8b4aa67ee402590abd0b9db827df6dd74ab3
jignesh1001 <jigneshguarv11@gmail.com>
2023-07-25 13:19:46 +0530

    ES6 modules

---
+++ Javascript learnings 2/javascript-modules/app.js
@@ -0,0 +1,9 @@
+import {firstName as fname} from './utils/fname.js'
+import {age} from './utils/age.js'
+import  something,{Person,Person2}  from './utils/person.js'
+
+// console.log(fname , age)
+
+const person = new Person("Jhon","Cena",22)
+console.log(person,something)
+person.info()
---
+++ Javascript learnings 2/javascript-modules/utils/fname.js
@@ -0,0 +1,2 @@
+const firstName = "Jhon"
+export {firstName}
